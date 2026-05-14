a7312ba4e1f5e0856817386d87cece17f2d70ca3
Mariana <oliiveira@gmail.com>
2026-05-14 10:04:27 -0300

    Deploy do Projeto Blog Pessoal

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'https://blog-pessoal-2-6xzz.onrender.com'
+    baseURL: import.meta.env.VITE_API_URL
 })
 
 // eslint-disable-next-line @typescript-eslint/no-wrapper-object-types
